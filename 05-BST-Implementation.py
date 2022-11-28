26f9bacf540bff7cd220dd7a10615b71f62ad226
monoloc0 <jue.schwinghammer@gmail.com>
2022-11-28 23:04:13 +0100

    bfs, dfs_pre_order, dfs_post_order, dfs_in_order

--- 05-BST-Implementation.py
+++ 05-BST-Implementation.py
@@ -56,6 +56,59 @@ class BinarySearchTree:
         
         return current_node
 
+    def BFS(self):
+
+        current_node = self.root
+        queue = []
+        queue.append(current_node)
+        results = []
+
+        while len(queue)>0:
+            current_node = queue.pop(0)
+            results.append(current_node.value)
+            if current_node.left is not None:
+                queue.append(current_node.left)
+            if current_node.right is not None:
+                queue.append(current_node.right)
+        return results
+
+    def dfs_pre_order(self):
+        results = []
+
+        def traverse(current_node):
+            results.append(current_node.value)
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+        traverse(self.root)
+        return results
+
+
+    def dfs_post_order(self):
+        results = []
+
+        def traverse(current_node):
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+            results.append(current_node.value)
+        traverse(self.root)
+        return results
+
+    def dfs_in_order(self):
+        results = []
+
+        def traverse(current_node):
+            if current_node.left is not None:
+                traverse(current_node.left)
+            results.append(current_node.value)
+            if current_node.right is not None:
+                traverse(current_node.right)   
+
+        traverse(self.root)
+        return results
 
 ####### Test Insert #######
 # my_tree = BinarySearchTree()
@@ -86,6 +139,88 @@ class BinarySearchTree:
 
 ####### Test min_value_node #######
 
+# my_tree = BinarySearchTree()
+# my_tree.insert(47)
+# my_tree.insert(21)
+# my_tree.insert(76)
+# my_tree.insert(18)
+# my_tree.insert(27)
+# my_tree.insert(52)
+# my_tree.insert(82)
+
+
+# print('Minimum Value in Tree:')
+# print( my_tree.min_value_node(my_tree.root).value )
+
+
+# ####### Test BFS #######
+# """
+#     EXPECTED OUTPUT:
+#     ----------------
+#     [47, 21, 76, 18, 27, 52, 82]
+
+#  """
+# my_tree = BinarySearchTree()
+# my_tree.insert(47)
+# my_tree.insert(21)
+# my_tree.insert(76)
+# my_tree.insert(18)
+# my_tree.insert(27)
+# my_tree.insert(52)
+# my_tree.insert(82)
+
+# print(my_tree.BFS())
+
+
+####### Test DFS PreOrder #######
+
+# """
+#     EXPECTED OUTPUT:
+#     ----------------
+#     [47, 21, 18, 27, 76, 52, 82]
+
+#  """
+
+# my_tree = BinarySearchTree()
+# my_tree.insert(47)
+# my_tree.insert(21)
+# my_tree.insert(76)
+# my_tree.insert(18)
+# my_tree.insert(27)
+# my_tree.insert(52)
+# my_tree.insert(82)
+
+# print(my_tree.dfs_pre_order())
+
+
+
+# ####### Test DFS PostOrder #######
+# """
+#     EXPECTED OUTPUT:
+#     ----------------
+#     [18, 27, 21, 52, 82, 76, 47]
+
+#  """
+# my_tree = BinarySearchTree()
+# my_tree.insert(47)
+# my_tree.insert(21)
+# my_tree.insert(76)
+# my_tree.insert(18)
+# my_tree.insert(27)
+# my_tree.insert(52)
+# my_tree.insert(82)
+
+# print(my_tree.dfs_post_order())
+
+
+####### Test DFS InOrder #######
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    [18, 21, 27, 47, 52, 76, 82]
+
+ """
+
 my_tree = BinarySearchTree()
 my_tree.insert(47)
 my_tree.insert(21)
@@ -95,16 +230,4 @@ my_tree.insert(27)
 my_tree.insert(52)
 my_tree.insert(82)
 
-
-print('Minimum Value in Tree:')
-print( my_tree.min_value_node(my_tree.root).value )
-
-            
-
-"""
-    EXPECTED OUTPUT:
-    ----------------
-    Minimum Value in Tree:
-    18
-
-"""
+print(my_tree.dfs_in_order())
